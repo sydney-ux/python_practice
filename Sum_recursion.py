ab63444d78e2b1cdb84e85ecb06516b92a3e64ce
sydney-ux <sydney.musyoka@strathmore.edu>
2025-04-26 14:14:36 +0300

    Update Sum_recursion.py

--- Sum_recursion.py
+++ Sum_recursion.py
@@ -3,7 +3,13 @@ def sum_recursion(n):
         print("NUMBER:1")
         return 1
     else:
-        return n + sum_recursion(n-1)
+        s_number = sum_recursion(n-1)
+        total = n + s_number
+        #Gives of the number and the sum
+        #it then continues till it reaches the last number/conditioin
+        print("the Sum of",n ,"and",s_number,"is",total)
+        return total
     
+#TOTALS THE FIRST 10 NUMBERS 
 
-print("Sum of first 10 numbers:", sum_recursion(10))
+print("Sum of first 10 numbers:", sum_recursion(8))
