1afa51db010771ece403e6c685f5aa30c90ad2f7
sydney-ux <sydney.musyoka@strathmore.edu>
2025-04-26 23:06:54 +0300

    Create evenorodd.py

---
+++ evenorodd.py
@@ -0,0 +1,7 @@
+#variable to store user input
+num = int(input("Enter a number: "))
+#check if the number is even or odd
+if num%2 == 0:
+    print("The number:",num," is even.")
+else:
+    print("The number:",num," is odd.")
